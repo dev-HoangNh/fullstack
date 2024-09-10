f8d46733d49e62e581eb5a5c101803f20991489e
Hoang NH <hh28122002@gmail.com>
2024-09-10 15:40:14 +0700

    second commit

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models";
+import CRUDServices from "../services/CRUDServices";
 
 let getHomePage = async (req, res) => {
     try {
@@ -16,7 +17,18 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs')
 }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs')
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDServices.createNewUser(req.body);
+    console.log(message)
+    return res.send('post crud from server')
+}
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
