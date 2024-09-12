b120c75b956a7b314a2373181c0a93fc595cd49b
Hoang NH <hh28122002@gmail.com>
2024-09-12 18:35:10 +0700

    4th commit

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -36,10 +36,36 @@ let displayGetCRUD = async (req, res) => {
         dataTable: data
     })
 }
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDServices.getUserInfoById(userId);
+
+        // let UserData
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    } else {
+        return res.send('edit crud')
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDServices.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+
+
 module.exports = {
     getHomePage: getHomePage,
     getAboutPage: getAboutPage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
-    displayGetCRUD: displayGetCRUD
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD
 }
